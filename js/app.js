0725af4ca8390ca9bbd1135fbbf4370db761ef08
Daniel Rodrigues <129228404+DanielSSRodrigues@users.noreply.github.com>
2023-04-06 16:00:42 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+console.log('Olá, Javascript!')
+
+var username = 'Daniel Rodrigues'
+
+document.getElementById('user-name').innerHTML = username //trás um elemento do html para o js - e atribuiu  o span do html a variavel no js
+
